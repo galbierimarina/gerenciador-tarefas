34dd56c05de8b8b14c9902914e5b06e849102130
Marina Galbieri <galbierimarina@gmail.com>
2026-07-01 12:03:46 -0300

    Implementa funçoes de adicionar e listar tarefas (CR do CRUD)

--- SRC/tarefas.py
+++ SRC/tarefas.py
@@ -13,3 +13,24 @@ class Tarefa:
 banco_de_tarefas = []
 
 print("Sistema de Gerenciamento de Tarefas inicializado com sucesso!")
+
+# Função para CRIAR (Create) uma nova tarefa
+def adicionar_tarefa(id_tarefa, titulo, descricao):
+    nova_tarefa = Tarefa(id_tarefa, titulo, descricao)
+    banco_de_tarefas.append(nova_tarefa)
+    print(f"Sucesso: A tarefa '{titulo}' foi adicionada!")
+
+# Função para LER/LISTAR (Read) as tarefas
+def listar_tarefas():
+    print("\n--- Lista de Tarefas da TechFlow ---")
+    if not banco_de_tarefas:
+        print("Nenhuma tarefa cadastrada no momento.")
+    else:
+        for tarefa in banco_de_tarefas:
+            print(tarefa)
+    print("------------------------------------\n")
+
+# --- Testando o nosso sistema ---
+adicionar_tarefa(1, "Configurar repositório", "Criar pastas src e testes")
+adicionar_tarefa(2, "Revisar uniformes", "Checar a nova política de vestimenta da equipe")
+listar_tarefas()
